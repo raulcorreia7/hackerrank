04dbaa5671b9521ac0be81545d7ab500b950a730
Raul Correia <raulcorreia7@gmail.com>
2019-09-06 00:48:10 +0100

    repeated string

---
+++ cpp/repeated_string/main.cpp
@@ -0,0 +1,58 @@
+#include <fstream>
+#include <string>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+long repeatedString(string s, long n) {
+    
+    long num_of_char_a = 0;
+    size_t string_size = s.size();
+    
+    auto count_lambda = [](string mystr, long limit)
+    {
+        long counter = 0;
+        for(size_t i=0; i < limit; ++i)
+        {
+            char c = mystr[i];
+            if(c == 'a')
+            {
+                ++counter;
+            }
+        }
+        return counter;
+    };
+
+    long num_repetitions = n / string_size;
+
+    num_of_char_a= num_repetitions * count_lambda(s,string_size);
+
+    if(num_repetitions % n != 0)
+    {
+        long dif = n - (num_repetitions * string_size);
+        num_of_char_a += count_lambda(s,dif);
+    }
+
+    return num_of_char_a;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string s;
+    getline(cin, s);
+
+    long n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    long result = repeatedString(s, n);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
