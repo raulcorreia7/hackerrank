471eebd253043de13c46d66e969474b2d9a73a8b
Raul Correia <raulcorreia7@gmail.com>
2019-09-06 19:50:55 +0100

    fix

--- cpp/repeated_string/main.cpp
+++ cpp/repeated_string/main.cpp
@@ -3,17 +3,16 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <stdint.h>
 using namespace std;
 
 long repeatedString(string s, long n)
 {
-
     long num_of_char_a = 0;
-    size_t string_size = s.size();
-
+    long string_size = s.size(); //string size is long max
     auto count_lambda = [](string mystr, long limit) {
         long counter = 0;
-        for (size_t i = 0; i < limit; ++i)
+        for (long i = 0; i < limit; ++i)
         {
             char c = mystr[i];
             if (c == 'a')
@@ -25,32 +24,30 @@ long repeatedString(string s, long n)
     };
 
     long num_repetitions = n / string_size;
-
     num_of_char_a = num_repetitions * count_lambda(s, string_size);
+
     if (num_repetitions % n != 0 || num_repetitions == 0)
     {
         long dif = n - (num_repetitions * string_size);
         num_of_char_a += count_lambda(s, dif);
     }
-
     return num_of_char_a;
 }
 
 int main()
 {
-    //ofstream fout(getenv("OUTPUT_PATH"));
+    ofstream fout(getenv("OUTPUT_PATH"));
 
-    string s = "ababa";
-    //getline(cin, s);
+    string s;
+    getline(cin, s);
 
-    long n = 3;
-    //cin >> n;
-    //cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    long n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
 
     long result = repeatedString(s, n);
 
-    //fout << result << "\n";
-    cout << result << "\n";
+    fout << result << "\n";
 
     return 0;
 }
