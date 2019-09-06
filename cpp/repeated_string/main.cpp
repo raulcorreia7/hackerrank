96cd0a8c9bbe0e543c51b19d5b3a223a92887ea2
Raul Correia <raulcorreia7@gmail.com>
2019-09-06 19:44:50 +0100

    fix

--- cpp/repeated_string/main.cpp
+++ cpp/repeated_string/main.cpp
@@ -5,18 +5,18 @@
 #include <algorithm>
 using namespace std;
 
-long repeatedString(string s, long n) {
-    
+long repeatedString(string s, long n)
+{
+
     long num_of_char_a = 0;
     size_t string_size = s.size();
-    
-    auto count_lambda = [](string mystr, long limit)
-    {
+
+    auto count_lambda = [](string mystr, long limit) {
         long counter = 0;
-        for(size_t i=0; i < limit; ++i)
+        for (size_t i = 0; i < limit; ++i)
         {
             char c = mystr[i];
-            if(c == 'a')
+            if (c == 'a')
             {
                 ++counter;
             }
@@ -26,12 +26,11 @@ long repeatedString(string s, long n) {
 
     long num_repetitions = n / string_size;
 
-    num_of_char_a= num_repetitions * count_lambda(s,string_size);
-
-    if(num_repetitions % n != 0)
+    num_of_char_a = num_repetitions * count_lambda(s, string_size);
+    if (num_repetitions % n != 0 || num_repetitions == 0)
     {
         long dif = n - (num_repetitions * string_size);
-        num_of_char_a += count_lambda(s,dif);
+        num_of_char_a += count_lambda(s, dif);
     }
 
     return num_of_char_a;
@@ -39,20 +38,19 @@ long repeatedString(string s, long n) {
 
 int main()
 {
-    ofstream fout(getenv("OUTPUT_PATH"));
+    //ofstream fout(getenv("OUTPUT_PATH"));
 
-    string s;
-    getline(cin, s);
+    string s = "ababa";
+    //getline(cin, s);
 
-    long n;
-    cin >> n;
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    long n = 3;
+    //cin >> n;
+    //cin.ignore(numeric_limits<streamsize>::max(), '\n');
 
     long result = repeatedString(s, n);
 
-    fout << result << "\n";
-
-    fout.close();
+    //fout << result << "\n";
+    cout << result << "\n";
 
     return 0;
 }
